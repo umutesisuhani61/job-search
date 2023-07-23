2f1156b0e31e6e478508f2d298c5844bc62a3b3c
Bamurange Amina <umutesisuhani61@gmail.com>
2023-07-23 17:49:14 +0200

    first commit

---
+++ api/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  APP_ID: "741a2d64",
+  API_KEY: "368200d0de7dc36cfee9227a0b6f8c06",
+  BASE_URL: 'https://api.adzuna.com/v1/api/jobs',
+  BASE_PARAMS: 'search/1?&results_per_page=20&content-type=application/json',
+};
